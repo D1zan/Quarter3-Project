35273c141e7d1a772894fa2f79e8bb3419796df4
sarah <student.189241@worcesterschools.net>
2025-04-02 13:41:31 -0400

    not dfone

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 My quartely project uses:
 -Lists; store city names, weather data
--API; Use a weather API to fetch current weather data
+#API; Use a weather API to fetch current weather data
 -Loops; Ask the user for the city name smd state until they press quit
 -Input Validation; Ensure the user only inputs a true existing city and state 
 
--- main.py
+++ main.py
@@ -2,19 +2,48 @@ import requests
 import json
 
 #creating the class 
-class weather:
-    def __innit__(self, city, temperature):
+class Weather:
+    def __innit__(self, city, visibility):
         self.city = city
-        self.temperature = temperature 
+        self.visibility = visibility 
 
     def display_info(self):
         print(f"City: {self.city}")
-        print(f"Temperature: {self.temperature}") 
+        print(f"Visibility: {self.visibility}") 
 #Getting the API
 
 def fetch_info(lat, lon):
-    url = f"https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid=b8712e4fd9e37511cf9f72ef17c1c6dc"
+    url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=b8712e4fd9e37511cf9f72ef17c1c6dc"
     response = requests.get(url)
-    return response
+    
+    if response.status_code == 200:
+        weather_data = response.json()
+        return weather_data
+    else:
+        print(f"\n This information is not available to you\n")
+#Albania, Tirana (41, 20)
+#SwiTzerland, Bern (47, 8)
+#Japan,Tokyo (35.67,139.65)
+#United States, Washington DC (38, -97)
+#Zocca, Rome (42.83, 12.83)
 
-print(fetch_info(35.67619190, 139.65031060))
+def info(weather_json):
+    weather = Weather(
+        weather_json["city"],
+        weather_json["visibility"]
+    )
+    return weather()
+
+#User input 
+
+print("Hey, Welcome to Weather App: \nIn this program you will choose a location from the given list, \nand the program will give you interesting weather information")
+#Loop
+
+while True:
+    print("These are the ones available: \n")
+    print("Tirana, Albania,\nBern, Switzerland,\nTokyo, Japan,\nWashington DC, United States,\nRome, Italy.")
+    cities = input("Which location would you like to choose?: ").lower().strip()
+    temp_info = fetch_info()
+
+    if not info: 
+        continue
