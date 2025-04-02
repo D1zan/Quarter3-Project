47743066c13e3c021c9d5da3677eb94b12f8e0d9
sarah <student.189241@worcesterschools.net>
2025-04-02 09:57:51 -0400

    j

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ class weather:
         print(f"Temperature: {self.temperature}") 
 #Getting the API
 
-def fetch_covid_info(city):
-    url = 
+def fetch_info(lat, lon):
+    url = f"https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid=b8712e4fd9e37511cf9f72ef17c1c6dc"
     response = requests.get(url)
+    return response
 
+print(fetch_info(35.67619190, 139.65031060))
