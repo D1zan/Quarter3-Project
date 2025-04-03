23c442b0a463d666d58918717051b4e5c025ca27
sarah <student.189241@worcesterschools.net>
2025-04-03 10:07:44 -0400

    done

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-My quartely project uses:
--Lists; store city names, weather data
+#-Lists; store city names, weather data
 #API; Use a weather API to fetch current weather data
--Loops; Ask the user for the city name smd state until they press quit
--Input Validation; Ensure the user only inputs a true existing city and state 
+#-Loops; Ask the user for the city name smd state until they press quit)
+#-Input Validation; Ensure the user only inputs a true existing city and state 
 
--- main.py
+++ main.py
@@ -1,49 +1,49 @@
-import requests 
+import requests
 import json
-
-#creating the class 
-class Weather:
-    def __innit__(self, city, visibility):
-        self.city = city
-        self.visibility = visibility 
-
-    def display_info(self):
-        print(f"City: {self.city}")
-        print(f"Visibility: {self.visibility}") 
-#Getting the API
 
 def fetch_info(lat, lon):
     url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=b8712e4fd9e37511cf9f72ef17c1c6dc"
     response = requests.get(url)
-    
+
     if response.status_code == 200:
-        weather_data = response.json()
-        return weather_data
+        return json.loads(response.content)
+       
     else:
-        print(f"\n This information is not available to you\n")
-#Albania, Tirana (41, 20)
-#SwiTzerland, Bern (47, 8)
-#Japan,Tokyo (35.67,139.65)
-#United States, Washington DC (38, -97)
-#Zocca, Rome (42.83, 12.83)
+        print("Error fetching weather data. Check you API for the 1000th time")
+        return None
 
-def info(weather_json):
-    weather = Weather(
-        weather_json["city"],
-        weather_json["visibility"]
-    )
-    return weather()
+locations = {
+    "tirana": (41, 20),
+    "bern": (47, 8),
+    "tokyo": (35.67, 139.65),
+    "washington DC": (38, -97),
+    "rome": (42.83, 12.83)
+}
 
-#User input 
+# User input
+print("\nWelcome to the Weather App!")
+print("Choose a location from the list below to get cool weather facts:\n")
 
-print("Hey, Welcome to Weather App: \nIn this program you will choose a location from the given list, \nand the program will give you interesting weather information")
-#Loop
-
+# Main Loop
 while True:
-    print("These are the ones available: \n")
-    print("Tirana, Albania,\nBern, Switzerland,\nTokyo, Japan,\nWashington DC, United States,\nRome, Italy.")
-    cities = input("Which location would you like to choose?: ").lower().strip()
-    temp_info = fetch_info()
+    for city in locations.keys():
+        print(city.title()) #method in python is used to capitalize the first letter of the word 
 
-    if not info: 
-        continue
+    user_choice = input("\nWhich location would you like to choose?: ").lower().strip()
+
+    if user_choice in locations:
+        lat, lon = locations[user_choice]
+        weather_data = fetch_info(lat, lon)
+
+        if weather_data:
+            forecast = weather_data["list"][0]  #grabbin the info from inside the dictionaries in the API
+            humidity = forecast["main"]["humidity"] #Humidity is inside main
+            temperature = forecast["main"]["temp"] #temp is inside main
+
+
+            print(f"\nWeather in: {user_choice}") 
+            print(f"- Temperature: {temperature - 273}°C")
+            print(f"- Humidity: {humidity}%")
+    keep_going = input("Would you like to ask about another country? (y/n):  ").lower().strip()
+    if keep_going == "n":
+        break  # Exit the loop 
